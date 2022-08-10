6b5b5ca1c19c7734e6f98ea6ec692bb18fd052a6
mohannad <mohannadrababah22@gmail.com>
2022-08-10 13:10:22 +0300

    --zaid login design

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,13 +1,14 @@
-import { Alert, Button, Container, Grid, Typography } from "@mui/material";
+import { Alert, Button, Container, Divider, Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import axios from "axios";
 import { TextField } from "mui-rff";
 import { useState } from "react";
 import { Form } from "react-final-form";
 import { useNavigate } from "react-router";
 
-const Login = ({setEmail}) => {
-    const nav=useNavigate()
-    const [errMsg,setErrMsg]=useState('')
+const Login = ({ setEmail }) => {
+  const nav = useNavigate()
+  const [errMsg, setErrMsg] = useState('')
   const onSubmit = (values) => {
     axios
       .post("https://pure-meadow-98451.herokuapp.com/login", {
@@ -16,12 +17,12 @@ const Login = ({setEmail}) => {
       })
       .then((res) => {
         console.log(res);
-        if(res.data.success){
-          localStorage.setItem('token',res.data.data.token)
+        if (res.data.success) {
+          localStorage.setItem('token', res.data.data.token)
           setEmail(values.email.toLowerCase().trim())
-          nav('/dashboard',{state:{email:values.email.toLowerCase().trim()}})
+          nav('/dashboard', { state: { email: values.email.toLowerCase().trim() } })
         }
-        else{
+        else {
           setErrMsg(res.data.message)
         }
       })
@@ -31,8 +32,21 @@ const Login = ({setEmail}) => {
   };
   return (
     <div >
-      <Container ><Typography marginTop={5} align='center' variant="h4">♛Royal Center♛</Typography></Container>
-        {errMsg&&<Alert sx={{width:'80%',margin:'auto'}} variant='outlined' severity='error'>{errMsg}</Alert>}
+      <Box sx={{backgroundColor:'black'}}>
+      <Grid container mt={3} minWidth={'500px'} >
+        
+        <Grid item xs={2} textAlign='right' mt={2}>
+          <img src='/logo-removebg-preview.png' style={{ height: '100px' }}></img>
+        </Grid>
+        <Grid item xs={8}>
+          <Typography marginTop={5} align='center' variant="h4" color='#e9ce7f'>Royal Center</Typography>
+        </Grid>
+        <Grid item xs={2} mt={2}>
+          <img src='/logo-removebg-preview.png' style={{ height: '100px' }}></img>
+        </Grid>
+      </Grid>
+      </Box>
+      {errMsg && <Alert sx={{ width: '80%', margin: 'auto' }} variant='outlined' severity='error'>{errMsg}</Alert>}
       <Container
         sx={{
           margin: "auto",
@@ -41,23 +55,25 @@ const Login = ({setEmail}) => {
           width: "40%",
           textAlign: "center",
           padding: "30px",
+          minWidth:'500px',
+          borderRadius:'10px'
         }}
       >
         <Form
-        
+
           onSubmit={onSubmit}
           render={({ handleSubmit }) => (
             <form onSubmit={handleSubmit}>
               <TextField
                 required
                 sx={{ margin: "10px" }}
-                label="userName"
+                label="User Name"
                 name="email"
               ></TextField>
               <TextField
                 required
                 sx={{ margin: "10px" }}
-                label="password"
+                label="Password"
                 name="password"
                 type='password'
               ></TextField>
